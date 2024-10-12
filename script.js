fb2a8766f5448f826ed4aae8b8dba589616337c5
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-12 02:40:04 -0700

    created a rock paper scissors game with 5 simulations

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("Helloooo Warudo");
 
 const value = ["rock", "paper", "scissors"];
 
+//plays 5 games of rock paper scissors
+playGame()
+
 function getComputerChoice() {
     let choice = value[Math.floor(Math.random() * value.length)]
     return choice
@@ -14,20 +17,29 @@ function getHumanChoice() {
     } else {getHumanChoice()}
 }
 
-let humanScore = 0, computerScore = 0
+function playGame() {
+    let humanScore = 0, computerScore = 0
 
-function playRound(humanChoice, computerChoice) {
-    console.log('You chose ' + humanChoice + ' and computer chose ' + computerChoice)
-    if (humanChoice == computerChoice) {
-        console.log("It's a tie.") // human and computer have the same choices
-    } else if (humanChoice == 'rock' && computerChoice == 'scissors' ||
-        humanChoice == 'scissors' && computerChoice == 'paper' ||
-        humanChoice == 'paper' && computerChoice == 'rock') {
-        console.log('You Win!') //human beats computer
-    } else {console.log('You Lose!')} //human loses to computer
+    for(let round = 1; round <= 5; round++) {
+        const human = getHumanChoice();
+        const computer = getComputerChoice();
+        playRound(human, computer)
+        
+        function playRound(human, computer) {
+            console.log('You chose ' + human + ' and computer chose ' + computer)
+            if (human == computer) {
+                console.log("It's a tie.") // human and computer have the same choices
+            } else if (human == 'rock' && computer == 'scissors' ||
+                human == 'scissors' && computer == 'paper' ||
+                human == 'paper' && computer == 'rock') {
+                humanScore++
+                console.log('You Win!') //human beats computer
+            } else { //human loses to computer
+                computer++
+                console.log('You Lose!')
+            } 
+        }
+    }
+    console.log("Score:\nYou: " + humanScore + "\nComputer: " + computerScore)
 }
 
-const computer = getComputerChoice();
-const human = getHumanChoice();
-
-playRound(human, computer);
