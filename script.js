59b7ea65f35b8e973da545906e51b87d251c281e
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-12 02:07:20 -0700

    created rock paper scissors game for a single round

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ const value = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let choice = value[Math.floor(Math.random() * value.length)]
-    return console.log(choice) 
+    return choice
 }
 
 function getHumanChoice() {
     let choice = prompt('Rock, Paper, Scissors:')
     if (value.includes(choice.toLowerCase())) {
-        return console.log(choice.toLowerCase())
+        return choice.toLowerCase()
     } else {getHumanChoice()}
 }
 
 let humanScore = 0, computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    console.log('You chose ' + humanChoice + ' and computer chose ' + computerChoice)
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie.") // human and computer have the same choices
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors' ||
+        humanChoice == 'scissors' && computerChoice == 'paper' ||
+        humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log('You Win!') //human beats computer
+    } else {console.log('You Lose!')} //human loses to computer
+}
+
+const computer = getComputerChoice();
+const human = getHumanChoice();
+
+playRound(human, computer);
