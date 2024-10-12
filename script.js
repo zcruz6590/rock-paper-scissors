a547bbbb0f48e10febc7adb682aadba87c00909c
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-12 00:12:21 -0700

    steps 1-3

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 console.log("Helloooo Warudo");
 
-function getComputerChoice() {}
-    let value = ["rock", "paper", "scissors"];
-    return value[Math.floor(Math.random * value.length)]
+const value = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let choice = value[Math.floor(Math.random() * value.length)]
+    return console.log(choice)
 }
 
+function getHumanChoice() {
+    let choice = prompt('Rock, Paper, Scissors:')
+    if (value.includes(choice.toLowerCase())) {
+        return console.log(choice.toLowerCase())
+    } else {getHumanChoice()}
+}
+
+
+getComputerChoice()
